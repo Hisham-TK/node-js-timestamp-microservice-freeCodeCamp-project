e275394322120cfc9431daf9f892ff44efa0492f
Hisham <hisham.taha.work+0@gmail.com>
2023-01-31 08:35:04 +0200

    init

--- index.js
+++ index.js
@@ -6,25 +6,34 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// your first API endpoint...
+app.get('/api/:date?', function (req, res) {
+  const { date: stringDate } = req.params;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  let numberDate;
+
+  if (stringDate) numberDate = parseInt(stringDate);
+
+  if (Number.isNaN(numberDate)) res.json({ error: 'Invalid date' });
+
+  let date;
+  if (numberDate) date = new Date(numberDate);
+  else date = new Date();
+
+  return res.json({ unix: date.getTime(), utc: date.toUTCString() });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
